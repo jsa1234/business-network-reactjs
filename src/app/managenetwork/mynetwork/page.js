d22d8150e3fb49b5b09885a032e9ed41d56db1c7
Ajal Jose <ajal.jose@olopo.app>
2024-10-31 12:04:12 +0530

    table with server side pagination implementation

--- src/app/managenetwork/mynetwork/page.js
+++ src/app/managenetwork/mynetwork/page.js
@@ -1,6 +1,7 @@
 import Pagenavigation from "@/components/Pagenavigation";
 import CustomPaginationActionsTable from "@/components/Test";
 import React from "react";
+import Mynetwork from "./Mynetwork";
 
 function Page() {
   const breadcrumbItems = ["Dashboard", "Manage Network", "My Networks"];
@@ -22,10 +23,28 @@ function Page() {
       <div className="w-full table-container mt-6">
         <div className="filter-group">
           <div className="form">
-          <svg className="fa fa-search" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path fillRule="evenodd" clipRule="evenodd" d="M8.5 13.5C11.2614 13.5 13.5 11.2614 13.5 8.5C13.5 5.73858 11.2614 3.5 8.5 3.5C5.73858 3.5 3.5 5.73858 3.5 8.5C3.5 11.2614 5.73858 13.5 8.5 13.5Z" stroke="#778294" strokeWidth="1.5"/>
-<path d="M12.0961 12.0961L16 16" stroke="#778294" strokeWidth="1.5" strokeLinecap="square"/>
-</svg>
+            <svg
+              className="fa fa-search"
+              width="20"
+              height="20"
+              viewBox="0 0 20 20"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M8.5 13.5C11.2614 13.5 13.5 11.2614 13.5 8.5C13.5 5.73858 11.2614 3.5 8.5 3.5C5.73858 3.5 3.5 5.73858 3.5 8.5C3.5 11.2614 5.73858 13.5 8.5 13.5Z"
+                stroke="#778294"
+                strokeWidth="1.5"
+              />
+              <path
+                d="M12.0961 12.0961L16 16"
+                stroke="#778294"
+                strokeWidth="1.5"
+                strokeLinecap="square"
+              />
+            </svg>
 
             <input
               type="text"
@@ -34,109 +53,8 @@ function Page() {
             />
           </div>
         </div>
-        {/* <table className="table w-full">
-          <thead>
-            <tr>
-              <th>Sl No</th>
-              <th>Product Name</th>
-              <th>Product Code</th>
-              <th>Total Suppliers</th>
-              <th>Total Retailers</th>
-              <th>No.of Customers</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>Widget A</td>
-              <td>WID-001</td>
-              <td>10</td>
-              <td>25</td>
-              <td>200</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>Gadget B</td>
-              <td>GAD-002</td>
-              <td>15</td>
-              <td>30</td>
-              <td>150</td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td>Device C</td>
-              <td>DEV-003</td>
-              <td>8</td>
-              <td>20</td>
-              <td>175</td>
-            </tr>
-            <tr>
-              <td>4</td>
-              <td>Thingamajig D</td>
-              <td>THI-004</td>
-              <td>12</td>
-              <td>22</td>
-              <td>180</td>
-            </tr>
-            <tr>
-              <td>5</td>
-              <td>Contraption E</td>
-              <td>CON-005</td>
-              <td>20</td>
-              <td>35</td>
-              <td>250</td>
-            </tr>
-            <tr>
-              <td>6</td>
-              <td>Apparatus F</td>
-              <td>APP-006</td>
-              <td>18</td>
-              <td>28</td>
-              <td>160</td>
-            </tr>
-            <tr>
-              <td>7</td>
-              <td>Gizmo G</td>
-              <td>GIZ-007</td>
-              <td>14</td>
-              <td>19</td>
-              <td>190</td>
-            </tr>
-            <tr>
-              <td>8</td>
-              <td>Tool H</td>
-              <td>TOO-008</td>
-              <td>16</td>
-              <td>27</td>
-              <td>210</td>
-            </tr>
-            <tr>
-              <td>9</td>
-              <td>Implement I</td>
-              <td>IMP-009</td>
-              <td>9</td>
-              <td>18</td>
-              <td>160</td>
-            </tr>
-            <tr>
-              <td>8</td>
-              <td>Tool H</td>
-              <td>TOO-008</td>
-              <td>16</td>
-              <td>27</td>
-              <td>210</td>
-            </tr>
-            <tr>
-              <td>9</td>
-              <td>Implement I</td>
-              <td>IMP-009</td>
-              <td>9</td>
-              <td>18</td>
-              <td>160</td>
-            </tr>
-          </tbody>
-        </table> */}
-        <CustomPaginationActionsTable/>
+        {/* <CustomPaginationActionsTable/> */}
+        <Mynetwork></Mynetwork>
       </div>
     </div>
   );
--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,50 +1,16 @@
-"use client"
-import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableFooter from '@mui/material/TableFooter';
-import TablePagination from '@mui/material/TablePagination';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import TableHead from '@mui/material/TableHead';
-import TablePaginationActions from './TablePagination';
+"use client";
+import * as React from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableFooter from "@mui/material/TableFooter";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import TableHead from "@mui/material/TableHead";
+import TablePaginationActions from "@/components/TablePagination";
 
-import Box from '@mui/material/Box';
-import Collapse from '@mui/material/Collapse';
-import IconButton from '@mui/material/IconButton';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0, 3.99),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3, 4.99),
-  createData('Eclair', 262, 16.0, 24, 6.0, 3.79),
-  createData('Cupcake', 305, 3.7, 67, 4.3, 2.5),
-  createData('Gingerbread', 356, 16.0, 49, 3.9, 1.5),
-];
-function createData(name, calories, fat, carbs, protein, price) {
-  return {
-    name,
-    calories,
-    fat,
-    carbs,
-    protein,
-    price,
-    history: [
-      {
-        date: '2020-01-05',
-        customerId: '11091700',
-        amount: 3,
-      },
-      {
-        date: '2020-01-02',
-        customerId: 'Anonymous',
-        amount: 1,
-      },
-    ],
-  };
-}
 function CustomPaginationActionsTable() {
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(5);
@@ -53,53 +19,81 @@ function CustomPaginationActionsTable() {
 
   const fetchData = async (currentPage, rowsPerPage) => {
     try {
-    //   const response = await fetch(
-    //     // Replace with your server endpoint URL
-    //     `your-api-endpoint?page=${currentPage + 1}&rowsPerPage=${rowsPerPage}`
-    //   );
+      //below code need to integrated when api are ready
+      //   const response = await fetch(
+      //     // Replace with your server endpoint URL
+      //     `your-api-endpoint?page=${currentPage + 1}&rowsPerPage=${rowsPerPage}`
+      //   );
 
-    //   if (!response.ok) {
-    //     throw new Error('Network response was not ok');
-    //   }
-      const data = await  {
-        "data": [
-          {
-            "id": 1,
-            "name": "Product A",
-            "price": 19.99,
-            "quantity": 10
-          },
-          {
-            "id": 2,
-            "name": "Product B",
-            "price": 29.99,
-            "quantity": 5
-          },
-          {
-            "id": 3,
-            "name": "Product C",
-            "price": 9.99,
-            "quantity": 20
-          },
-          {
-            "id": 4,
-            "name": "Product D",
-            "price": 39.99,
-            "quantity": 8
-          },
-          {
-            "id": 5,
-            "name": "Product E",
-            "price": 14.99,
-            "quantity": 15
-          }
-        ],
-        "totalCount": 5
-      };
-      setData(data.data);
-      setTotalCount(data.totalCount);
+      //   if (!response.ok) {
+      //     throw new Error('Network response was not ok');
+      //   }
+      const rows = [
+        {
+          name: "Frozen yoghurt",
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          price: 3.99,
+          history: [
+            { date: "2020-01-05", customerId: "11091700", amount: 3 },
+            { date: "2020-01-02", customerId: "Anonymou5", amount: 1 },
+          ],
+        },
+        {
+          name: "Ice cream sandwich",
+          calories: 237,
+          fat: 9.0,
+          carbs: 37,
+          protein: 4.3,
+          price: 4.99,
+          history: [
+            { date: "2020-01-05", customerId: "11091700", amount: 3 },
+            { date: "2020-01-02", customerId: "Anonymous4", amount: 1 },
+          ],
+        },
+        {
+          name: "Eclair",
+          calories: 262,
+          fat: 16.0,
+          carbs: 24,
+          protein: 6.0,
+          price: 3.79,
+          history: [
+            { date: "2020-01-05", customerId: "11091700", amount: 3 },
+            { date: "2020-01-02", customerId: "Anonymous3", amount: 1 },
+          ],
+        },
+        {
+          name: "Cupcake",
+          calories: 305,
+          fat: 3.7,
+          carbs: 67,
+          protein: 4.3,
+          price: 2.5,
+          history: [
+            { date: "2020-01-05", customerId: "11091700", amount: 3 },
+            { date: "2020-01-02", customerId: "Anonymous2", amount: 1 },
+          ],
+        },
+        {
+          name: "Gingerbread",
+          calories: 356,
+          fat: 16.0,
+          carbs: 49,
+          protein: 3.9,
+          price: 1.5,
+          history: [
+            { date: "2020-01-05", customerId: "11091700", amount: 3 },
+            { date: "2020-01-02", customerId: "Anonymous1", amount: 1 },
+          ],
+        },
+      ];
+      setData(rows);
+      setTotalCount(5); //value need to be set from api
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error("Error fetching data:", error);
       // Handle error, e.g., display error message to the user
     }
   };
@@ -119,26 +113,36 @@ function CustomPaginationActionsTable() {
 
   return (
     <TableContainer component={Paper}>
-    <Table aria-label="collapsible table">
-      <TableHead>
-        <TableRow>
-          <TableCell>Dessert (100g serving)</TableCell>
-          <TableCell align="right">Calories</TableCell>
-          <TableCell align="right">Fat&nbsp;(g)</TableCell>
-          <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-          <TableCell align="right">Protein&nbsp;(g)</TableCell>
-          <TableCell />
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {rows.map((row) => (
-          <Row key={row.name} row={row} />
-        ))}
-      </TableBody>
-      <TableFooter>
+      <Table className="table" aria-label="collapsible table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell align="right">Calories</TableCell>
+            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {data.map((row) => (
+            <TableRow
+              sx={{ "& > *": { borderBottom: "unset" } }}
+              key={row.name}
+            >
+              <TableCell component="td" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+        <TableFooter>
           <TableRow>
             <TablePagination
-              rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+              rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
               colSpan={6}
               count={totalCount}
               rowsPerPage={rowsPerPage}
@@ -146,7 +150,7 @@ function CustomPaginationActionsTable() {
               slotProps={{
                 select: {
                   inputProps: {
-                    'aria-label': 'rows per page',
+                    "aria-label": "rows per page",
                   },
                   native: true,
                 },
@@ -157,70 +161,8 @@ function CustomPaginationActionsTable() {
             />
           </TableRow>
         </TableFooter>
-    </Table>
-  </TableContainer>
-  );
-}
-function Row(props) {
-  const { row } = props;
-  const [open, setOpen] = React.useState(false);
-
-  return (
-    <React.Fragment>
-      <TableRow sx={{ '& > *': { borderBottom: 'unset' } }}>
-        
-        <TableCell component="th" scope="row">
-          {row.name}
-        </TableCell>
-        <TableCell align="right">{row.calories}</TableCell>
-        <TableCell align="right">{row.fat}</TableCell>
-        <TableCell align="right">{row.carbs}</TableCell>
-        <TableCell align="right">{row.protein}</TableCell>
-        <TableCell>
-          <IconButton
-            aria-label="expand row"
-            size="small"
-            onClick={() => setOpen(!open)}
-          >
-            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-          </IconButton>
-        </TableCell>
-      </TableRow>
-      <TableRow>
-        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-          <Collapse in={open} timeout="auto" unmountOnExit>
-            <Box sx={{ margin: 1 }}>
-           
-                History
-              <Table size="small" aria-label="purchases">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Date</TableCell>
-                    <TableCell>Customer</TableCell>
-                    <TableCell align="right">Amount</TableCell>
-                    <TableCell align="right">Total price ($)</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {row.history.map((historyRow) => (
-                    <TableRow key={historyRow.date}>
-                      <TableCell component="th" scope="row">
-                        {historyRow.date}
-                      </TableCell>
-                      <TableCell>{historyRow.customerId}</TableCell>
-                      <TableCell align="right">{historyRow.amount}</TableCell>
-                      <TableCell align="right">
-                        {Math.round(historyRow.amount * row.price * 100) / 100}
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </Box>
-          </Collapse>
-        </TableCell>
-      </TableRow>
-    </React.Fragment>
+      </Table>
+    </TableContainer>
   );
 }
 export default CustomPaginationActionsTable;
