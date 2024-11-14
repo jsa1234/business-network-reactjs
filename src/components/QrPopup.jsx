b2c76fe1e441126fbe91115d8730517f2cde4b1f
Ajal Jose <ajal.jose@olopo.app>
2024-11-14 17:19:05 +0530

    qrrecieved

--- src/components/QrPopup.jsx
+++ src/components/QrPopup.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Cancel from "../../public/assests/icons/cancel.svg";
 import Navigation from "../../public/assests/icons/navigation.svg";
 import Calendar from "../../public/assests/icons/calendar.svg";
-const QrPopup = ({ showModal, handleModalClose }) => {
+const QrPopup = ({ showModal, handleModalClose,handleSubmit,dateChange }) => {
   const sendValue = () => {
     handleModalClose(false); // Call the parent's function
   };
@@ -16,13 +16,13 @@ const QrPopup = ({ showModal, handleModalClose }) => {
         <p>Details has been successfully submitted. Thanks!</p>
         <div className="input__group mt-10">
           <label htmlFor="BusinessInpt">Expected Delivery Date</label>
-          <input type="date" id="BusinessInpt" />
+          <input type="date" id="BusinessInpt" onChange={(e)=>dateChange(e.target.value)}/>
         </div>
         <div className="input__group">
           <label htmlFor="commentRow">Comments</label>
           <textarea rows={3} id="commentRow" />
         </div>
-        <button className="green__btn">
+        <button className="green__btn" onClick={()=>handleSubmit()}>
           <Navigation />
           Submit
         </button>
