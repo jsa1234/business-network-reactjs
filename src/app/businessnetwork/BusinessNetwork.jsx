6af7f877ebede710d1ff0f613185e2115061a210
christy-olopo <christy.david@olopo.app>
2024-11-14 17:24:26 +0530

    BusinessNetwork API integration

--- src/app/businessnetwork/BusinessNetwork.jsx
+++ src/app/businessnetwork/BusinessNetwork.jsx
@@ -4,6 +4,7 @@ import ChevronIcon from "../../../public/assests/icons/chevron-right.svg";
 import SearchIcon from "../../../public/assests/icons/search_btn.svg";
 import BNcard from "@/components/BNcard";
 import Popup from "@/components/Popup";
+import CommonApi from "@/api/CommonApi";
 
 const BusinessNetwork = () => {
   const [page, setPage] = useState(0);
@@ -91,6 +92,10 @@ const BusinessNetwork = () => {
   const [location, setLocation] = useState("");
   const [rating, setRating] = useState("");
   const [showModal, setshowModal] = useState(true);
+  const [productCategoryData, setProductCategoryData] = useState([]);
+  const [vendorCategoryData, setVendorCategoryData] = useState([]);
+  const [locationsData, setLoationsData] = useState([]);
+  const [ratingsData, setRatingsData] = useState([]);
   // Handle input changes
   const handleInputChange = (e, setterFunction) => {
     setterFunction(e.target.value);
@@ -100,112 +105,210 @@ const BusinessNetwork = () => {
   const handleSearch = (e) => {
     e.preventDefault();
     console.log({
-      gstNo,
-      mobileNo,
-      businessName,
-      productCategory,
-      vendorCategory,
-      location,
-      rating,
+      VendorUUId: "21C7586F-9F29-457B-8E3D-4C75213183DF",
+      gstNo: "",
+      mobileNo: "",
+      businessName: "",
+      productCategory: "",
+      vendorCategory: "1",
+      location: "",
+      rating: "",
     });
     // Add your search logic here
   };
-  const handleMinimize=()=>{
+  const handleMinimize = () => {
     setshowModal(!showModal);
+  };
+
+  useEffect(() => {
+    getCategories();
+    getVendors();
+    getLocations();
+    getRatings();
+  }, []);
+
+  // product category api
+  async function getCategories() {
+    try {
+      const data = await CommonApi.getData(
+        "BusinessNetwork/vendor/product/categories",
+        {},
+        {}
+      );
+      setProductCategoryData((prevState) => [...prevState, ...data]);
+      // console.log(productCategoryData);
+    } catch (error) {
+      console.error("Error fetching network data:", error);
+    }
   }
+
+  // vendor category api
+  async function getVendors() {
+    try {
+      const data = await CommonApi.getData(
+        "BusinessNetwork/vendor/categories",
+        {},
+        {}
+      );
+      setVendorCategoryData((prevState) => [...prevState, ...data]);
+    } catch (error) {
+      console.error("Error fetching network data:", error);
+    }
+  }
+
+  // vendor location api
+  async function getLocations() {
+    try {
+      const data = await CommonApi.getData(
+        "BusinessNetwork/vendor/locations",
+        {},
+        {}
+      );
+      setLoationsData((prevState) => [...prevState, ...data]);
+    } catch (error) {
+      console.error("Error fetching network data:", error);
+    }
+  }
+
+  // vendor ratings api
+  async function getRatings() {
+    try {
+      const data = await CommonApi.getData(
+        "BusinessNetwork/vendor/ratings",
+        {},
+        {}
+      );
+      setRatingsData((prevState) => [...prevState, ...data]);
+    } catch (error) {
+      console.error("Error fetching network data:", error);
+    }
+  }
+
   return (
     <div>
       <div className="advance__filter">
         <div className="advance__filter__header">
           <h1>Advance Filter</h1>
-          <ChevronIcon onClick={()=>handleMinimize()} className={!showModal?"rotate":""}/>
+          <ChevronIcon
+            onClick={() => handleMinimize()}
+            className={!showModal ? "rotate" : ""}
+          />
         </div>
         <hr></hr>
-        {showModal?<div className="advance__filter__body grid grid-cols-12 gap-4">
-          <div className="input__group col-span-3">
-            <label htmlFor="gstSrchInpt">By GST No.</label>
-            <input
-              type="text"
-              id="gstSrchInpt"
-              value={gstNo}
-              onChange={(e) => handleInputChange(e, setGstNo)}
-            />
+        {showModal ? (
+          <div className="advance__filter__body grid grid-cols-12 gap-4">
+            <div className="input__group col-span-3">
+              <label htmlFor="gstSrchInpt">By GST No.</label>
+              <input
+                type="text"
+                id="gstSrchInpt"
+                value={gstNo}
+                onChange={(e) => handleInputChange(e, setGstNo)}
+              />
+            </div>
+            <div className="input__group col-span-3">
+              <label htmlFor="MobileInpt">By Mobile No.</label>
+              <input
+                type="text"
+                id="MobileInpt"
+                maxLength="10"
+                value={mobileNo}
+                onChange={(e) => handleInputChange(e, setMobileNo)}
+              />
+            </div>
+            <div className="input__group col-span-3">
+              <label htmlFor="BusinessInpt">By Business Name</label>
+              <input
+                type="text"
+                id="BusinessInpt"
+                value={businessName}
+                onChange={(e) => handleInputChange(e, setBusinessName)}
+              />
+            </div>
+            <div className="input__group col-span-3">
+              <label htmlFor="ProductInpt">By Product Category</label>
+              <select
+                id="ProductInpt"
+                value={productCategory}
+                onChange={(e) => handleInputChange(e, setProductCategory)}
+              >
+                <option value="">Select from the list</option>
+
+                {productCategoryData.map((item, index) => {
+                  return (
+                    <option value={item.categoryName} key={index}>
+                      {item.categoryName}
+                    </option>
+                  );
+                })}
+              </select>
+            </div>
+            <div className="input__group col-span-3">
+              <label htmlFor="VendorInpt">By Vendor Category</label>
+              <select
+                id="VendorInpt"
+                value={vendorCategory}
+                onChange={(e) => handleInputChange(e, setVendorCategory)}
+              >
+                <option value="">Select from the list</option>
+                {vendorCategoryData.map((item, index) => {
+                  return (
+                    <option value={item.categoryName} key={index}>
+                      {item.categoryName}
+                    </option>
+                  );
+                })}
+              </select>
+            </div>
+            <div className="input__group col-span-3">
+              <label htmlFor="LocationInpt">By Location</label>
+              <select
+                id="LocationInpt"
+                value={location}
+                onChange={(e) => handleInputChange(e, setLocation)}
+              >
+                <option value="">Select from the list</option>
+                {locationsData.map((item, index) => {
+                  return (
+                    <option value={item.name} key={index}>
+                      {item.name}
+                    </option>
+                  );
+                })}
+              </select>
+            </div>
+            <div className="input__group col-span-3">
+              <label htmlFor="RatingInpt">By Rating</label>
+              <select
+                id="RatingInpt"
+                value={rating}
+                onChange={(e) => handleInputChange(e, setRating)}
+              >
+                <option value="">Select from the list</option>
+                {ratingsData.map((item, index) => {
+                  return (
+                    <option value={item.name} key={index}>
+                      {item.name}
+                    </option>
+                  );
+                })}
+              </select>
+            </div>
+            <div className="input__group col-span-3">
+              <p>&nbsp;</p>
+              <button
+                id="search_btn"
+                className="primary__btn flex pl-20 pr-20 gap-1 justify-around"
+                onClick={handleSearch}
+              >
+                Search Now
+                <SearchIcon />
+              </button>
+            </div>
           </div>
-          <div className="input__group col-span-3">
-            <label htmlFor="MobileInpt">By Mobile No.</label>
-            <input
-              type="text"
-              id="MobileInpt"
-              maxLength="10"
-              value={mobileNo}
-              onChange={(e) => handleInputChange(e, setMobileNo)}
-            />
-          </div>
-          <div className="input__group col-span-3">
-            <label htmlFor="BusinessInpt">By Business Name</label>
-            <input
-              type="text"
-              id="BusinessInpt"
-              value={businessName}
-              onChange={(e) => handleInputChange(e, setBusinessName)}
-            />
-          </div>
-          <div className="input__group col-span-3">
-            <label htmlFor="ProductInpt">By Product Category</label>
-            <select
-              id="ProductInpt"
-              value={productCategory}
-              onChange={(e) => handleInputChange(e, setProductCategory)}
-            >
-              <option value="">Select from the list</option>
-              {/* Add options dynamically if needed */}
-            </select>
-          </div>
-          <div className="input__group col-span-3">
-            <label htmlFor="VendorInpt">By Vendor Category</label>
-            <select
-              id="VendorInpt"
-              value={vendorCategory}
-              onChange={(e) => handleInputChange(e, setVendorCategory)}
-            >
-              <option value="">Select from the list</option>
-              {/* Add options dynamically if needed */}
-            </select>
-          </div>
-          <div className="input__group col-span-3">
-            <label htmlFor="LocationInpt">By Location</label>
-            <select
-              id="LocationInpt"
-              value={location}
-              onChange={(e) => handleInputChange(e, setLocation)}
-            >
-              <option value="">Select from the list</option>
-              {/* Add options dynamically if needed */}
-            </select>
-          </div>
-          <div className="input__group col-span-3">
-            <label htmlFor="RatingInpt">By Rating</label>
-            <select
-              id="RatingInpt"
-              value={rating}
-              onChange={(e) => handleInputChange(e, setRating)}
-            >
-              <option value="">Select from the list</option>
-              {/* Add options dynamically if needed */}
-            </select>
-          </div>
-          <div className="input__group col-span-3">
-            <p>&nbsp;</p>
-            <button
-              id="search_btn"
-              className="primary__btn flex pl-20 pr-20 gap-1 justify-around"
-              onClick={handleSearch}
-            >
-              Search Now
-              <SearchIcon />
-            </button>
-          </div>
-        </div>:''}
+        ) : (
+          ""
+        )}
       </div>
       <div className="filter__results">
         <div className="filter__results__header">
