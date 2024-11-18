4b35c1b0e5d09518b82b828a4a8134e93aaefac7
christy-olopo <christy.david@olopo.app>
2024-11-18 11:57:03 +0530

    Implemented redux store

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,23 @@
+"use client";
+
 import Dashboard from "@/components/Dashboard";
 import Pagenavigation from "@/components/Pagenavigation";
+import { useSelector } from "react-redux";
 
 function Page() {
+  const VendorMasterUUID = useSelector(
+    (state) => state.vendor.VendorMasterUUID
+  );
   const urlList = ["/", "/dashboard", ""];
+
   return (
-    
-     <div className="bus__body w-full pl-9 mt-6 pr-3">
-     <div className="flex justify-between">
-     <div className="w-full md:w-1/2">
+    <div className="bus__body w-full pl-9 mt-6 pr-3">
+      <div>
+        <h1>VendorMasterUUID: {VendorMasterUUID}</h1>
+      </div>
+
+      <div className="flex justify-between">
+        <div className="w-full md:w-1/2">
           <Pagenavigation
             pageName="Dashboard"
             message="Hi, MS Market. Welcome back to Olopo"
@@ -15,12 +25,10 @@ function Page() {
         </div>
       </div>
       <div className="w-full mt-6">
-
-          <Dashboard/>
+        <Dashboard />
       </div>
-     </div>
-    
-);
+    </div>
+  );
 }
 
 export default Page;
---
+++ src/store/vendorSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  VendorMasterUUID: "21C7586F-9F29-457B-8E3D-4C75213183DF", // Default value
+};
+
+const vendorSlice = createSlice({
+  name: "vendor",
+  initialState,
+  reducers: {
+    setVendorMasterUUID: (state, action) => {
+      state.VendorMasterUUID = action.payload;
+    },
+    resetVendorMasterUUID: (state) => {
+      state.VendorMasterUUID = "";
+    },
+  },
+});
+
+export const { setVendorMasterUUID, resetVendorMasterUUID } =
+  vendorSlice.actions;
+
+export default vendorSlice.reducer;
