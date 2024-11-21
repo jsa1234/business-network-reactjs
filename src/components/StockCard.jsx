71e76bfe5168ee6f61238c47f5605cc1b0f49768
Christy David Cruze <christy.david.cruze@gmail.com>
2024-11-21 09:33:35 +0530

    Dashboard update

--- src/components/StockCard.jsx
+++ src/components/StockCard.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import Package from "../../public/assests/icons/package.svg";
-import ArrowRight from "../../public/assests/icons/arrow-right.svg";
+import React from "react"
+import Package from "../../public/assests/icons/package.svg"
+import ArrowRight from "../../public/assests/icons/arrow-right.svg"
 const StockCard = ({ mode, btnClick, data }) => {
   return (
     <div
@@ -13,22 +13,22 @@ const StockCard = ({ mode, btnClick, data }) => {
       } col-span-4`}
       onClick={btnClick}
     >
-      <div className="stock__content">
+      <div className='stock__content'>
         <p>STOCK STATUS</p>
         <Package />
-        <div className="stock_total_user">
+        <div className='stock_total_user'>
           <h4>{mode == "medium" ? "Total Buyers" : "Total Suppliers"}</h4>
-          <h1>{data.totalSuppliers}</h1>
+          <h1>{data?.totalSuppliers}</h1>
         </div>
-        <div className="vl"></div>
-        <div className="stock_total_user">
+        <div className='vl'></div>
+        <div className='stock_total_user'>
           <h4>Total Products</h4>
-          <h1>{data.totalProducts}</h1>
+          <h1>{data?.totalProducts}</h1>
         </div>
       </div>
       <ArrowRight />
     </div>
-  );
-};
+  )
+}
 
-export default StockCard;
+export default StockCard
