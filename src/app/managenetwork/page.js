599b246c445ae482e1ea62044544e6c840571ac1
christy-olopo <christy.david@olopo.app>
2024-11-25 18:03:28 +0530

    Fixed error in modal.jsx

--- src/app/managenetwork/page.js
+++ src/app/managenetwork/page.js
@@ -1,83 +1,23 @@
-"use client";
-import Buttons from "@/components/Buttons/Buttons";
 import Pagenavigation from "@/components/Pagenavigation";
-import { useState } from "react";
-import Link from "next/link";
-import { Tabs } from "@mui/material";
-import Managework from "./managenetwork/Managework";
-import Approvalpending from "../../../public/assests/icons/approvalpending.svg";
-import Requestpending from "../../../public/assests/icons/requestpending.svg";
-import Viewnetwork from "../../../public/assests/icons/viewnetwork.svg";
-import Network from "../../../public/assests/icons/network.svg"
-function Page() {
-  const breadcrumbItems = ["Dashboard", "Manage Networks"];
-  const urlList = ["/", "/dashboard", ""];
-  const [selectedOption, setSelectedOption] = useState("");
-  const [activeTab, setActiveTab] = useState("approval");
+import React, { Suspense } from "react";
+import BusinessNetwork from "./BusinessNetwork";
 
-  const handleChange = (event) => {
-    setSelectedOption(event.target.value);
-  };
+function Page() {
   return (
     <div className="bus__body w-full pl-9 mt-6 pr-3">
       <div className="flex justify-between">
         <div className="w-full md:w-1/2">
           <Pagenavigation
-            pageName="Manage Networks"
-            navList={breadcrumbItems}
-            urlList={urlList}
+            pageName="Build My Network Connections"
+            message="Hi, MS Market. Welcome back to Olopo"
           />
         </div>
-        <div className="w-full md:w-1/2 flex justify-end">
-         {/*  <Link href="/managenetwork/mynetwork">
-            <button className="primary__btn">
-              <Viewnetwork />
-              View My Networks
-            </button>
-          </Link> */}
-        </div>
       </div>
-
-      {/* Tabs */}
-      <div>
-        <div className="flex  mt-6 background">
-        <button
-            className={`tab flex items-center justify-center gap-2 p-2 rounded-md ${
-              activeTab === "network"
-                ? "bg-white text-orange-500 border-b-2 border-b-orange-500 active"
-                : ""
-            }`}
-            onClick={() => setActiveTab("network")}
-          >
-            <Network />
-            <span>My Network</span>
-          </button>
-          <button
-            className={`tab flex items-center justify-center gap-2 p-2 rounded-md ${
-              activeTab === "approval"
-                ? "bg-white text-orange-500 border-b-2 border-b-orange-500 active"
-                : ""
-            }`}
-            onClick={() => setActiveTab("approval")}
-          >
-            <Approvalpending />
-            <span>Approval Pending</span>
-          </button>
-          <button
-            className={`tab flex items-center justify-center p-2 rounded-md gap-2  ${
-              activeTab === "request"
-                ? "bg-white text-orange-500 border-b-2 border-b-orange-500 active"
-                : ""
-            }`}
-            onClick={() => setActiveTab("request")}
-          >
-            <Requestpending />
-            <span>Request Pending</span>
-          </button>
-        </div>
-
-        {/*  {renderTableData()} */}
-        <Managework activeTab={activeTab} />
+      <div className="w-full mt-6">
+        {/* Wrap BusinessNetwork with Suspense */}
+        <Suspense fallback={<div>Loading Business Network...</div>}>
+          <BusinessNetwork />
+        </Suspense>
       </div>
     </div>
   );
--- src/app/managenetwork/search/page.js
+++ src/app/managenetwork/search/page.js
@@ -1,51 +1,53 @@
 "use client";
 import { useSearchParams } from "next/navigation";
-import { useEffect, useState,Suspense } from "react";
+import { useEffect, useState, Suspense } from "react";
 import Pagenavigation from "@/components/Pagenavigation";
 import SearchTable from "./SearchTable";
 import Search from "../../../../public/assests/icons/search.svg";
 function Page() {
-  const [param,setParam]=useState('');
+  const [param, setParam] = useState("");
   const searchParams = useSearchParams();
   useEffect(() => {
-    
     const myProp = searchParams.get("myProp");
     setParam(atob(myProp));
-    console.log(myProp)
+    console.log(myProp);
   }, []);
 
-  return <div className="bus__body w-full pl-9 mt-6 pr-3 pb-9">
-<Suspense fallback={<div>Loading search results...</div>}>
-    <div className="flex justify-between">
-        <div className="w-full">
-          <Pagenavigation
-            pageName={
+  return (
+    <div className="bus__body w-full pl-9 mt-6 pr-3 pb-9">
+      <Suspense fallback={<div>Loading search results...</div>}>
+        <div className="flex justify-between">
+          <div className="w-full">
+            <Pagenavigation
+              pageName={
                 <>
-                    Search Result of <span className='highlight_txt'>{param}</span>
+                  Search Result of{" "}
+                  <span className="highlight_txt">{param}</span>
                 </>
-            }
-            navList={[]}
-            urlList={[]}
-            message="Connect from the list"
-          />
-        </div>
-      </div>
-      <div className="w-full table-container mt-6">
-{/* <CustomPaginationActionsTable/> */}
-<div className="filter-group">
-          <div className="form">
-            <Search className="fa fa-search"></Search>
-
-            <input
-              type="text"
-              className="form-control form-input"
-              placeholder="Search Product Name..."
+              }
+              navList={[]}
+              urlList={[]}
+              message="Connect from the list"
             />
           </div>
         </div>
-       <SearchTable searchValue={param}></SearchTable>
-      </div>
+        <div className="w-full table-container mt-6">
+          {/* <CustomPaginationActionsTable/> */}
+          <div className="filter-group">
+            <div className="form">
+              <Search className="fa fa-search"></Search>
+
+              <input
+                type="text"
+                className="form-control form-input"
+                placeholder="Search Product Name..."
+              />
+            </div>
+          </div>
+          <SearchTable searchValue={param}></SearchTable>
+        </div>
       </Suspense>
-  </div>;
+    </div>
+  );
 }
 export default Page;
