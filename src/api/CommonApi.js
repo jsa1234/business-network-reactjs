4363b3b03eea88d43cb6b1ab4e28c56e5faab39a
christy-olopo <christy.david@olopo.app>
2024-11-28 18:58:42 +0530

    Hard-coded vendormasteruuid

--- src/api/CommonApi.js
+++ src/api/CommonApi.js
@@ -8,7 +8,7 @@ class CommonAPI {
     Authorization: `Bearer ${process.env.NEXT_PUBLIC_API_TOKEN}`, // If you need an Authorization token
   };
   getData(url, headers = {},params = {}) {
-    console.log(process.env.NEXT_PUBLIC_API_URL);
+    // console.log(process.env.NEXT_PUBLIC_API_URL);
     const combinedHeaders = { ...this.defaultHeaders, ...headers };
     return axios
       .get(`${process.env.NEXT_PUBLIC_API_URL}/${url}`, { headers: combinedHeaders,  params: params}).then((response) => {
@@ -26,10 +26,10 @@ class CommonAPI {
     // POST request with body and optional headers
     postData(url,headers = {}, data) {
       const combinedHeaders = { ...this.defaultHeaders, ...headers };
-      console.log("Making POST request to:", `${process.env.NEXT_PUBLIC_API_URL}/${url}`);
+      // console.log("Making POST request to:", `${process.env.NEXT_PUBLIC_API_URL}/${url}`);
       return axios.post(`${process.env.NEXT_PUBLIC_API_URL}/${url}`, data, { headers: combinedHeaders })
         .then((response) => {
-          console.log("POST response data:", response.data);
+          // console.log("POST response data:", response.data);
           return response.data;
         })
         .catch((error) => {
@@ -38,11 +38,11 @@ class CommonAPI {
     }
     putData(url, headers = {}, data) {
       const combinedHeaders = { ...this.defaultHeaders, ...headers };
-      console.log("Making PUT request to:", `${process.env.NEXT_PUBLIC_API_URL}/${url}`);
+      // console.log("Making PUT request to:", `${process.env.NEXT_PUBLIC_API_URL}/${url}`);
       return axios
         .put(`${process.env.NEXT_PUBLIC_API_URL}/${url}`, data, { headers: combinedHeaders })
         .then((response) => {
-          console.log("PUT response data:", response);
+          // console.log("PUT response data:", response);
           if(response.status==200){
             return {status:"success"};
           }
