babb1737c2ca2be607d3da7b8c76ba2d76c1678a
Ajal Jose <ajal.jose@olopo.app>
2024-12-03 11:50:46 +0530

    api

--- src/components/Rejectpopup.jsx
+++ src/components/Rejectpopup.jsx
@@ -74,8 +74,8 @@ const Rejectpopup = ({ handleModalClose,busid }) => {
   //end//
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50 ">
-      <div className="bg-white p-6 rounded-lg w-[55rem]  relative">
+    <div className="qr-modal fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50 ">
+      <div className="modal-content bg-white p-6 rounded-lg w-[55rem]  relative">
         <button
           onClick={handleModalClose}
           className="absolute top-2 right-4 text-4xl font-bold text-gray-500"
